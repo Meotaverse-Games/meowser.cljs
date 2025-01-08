4dcf3baf3df2774f6d2fcf2ab777e07626e502ba
Tokusei Noborio <tokusei@nyampass.com>
2025-01-08 21:40:34 +0900

    boot parameter for gen-game

--- src/main/meowser/core.cljs
+++ src/main/meowser/core.cljs
@@ -2,21 +2,21 @@
   (:require [integrant.core :as ig]
             ["phaser" :as phaser]))
 
-(defn gen-game [{:keys [debug? width height scenes gravity] :or {debug? false}}]
-  (let [config
-        (clj->js {:type (.-WEBGL phaser)
-                  :parent "root"
-                  :width (.-innerWidth js/window)
-                  :height (.-innerHeight js/window)
-                  :pixelArt false
-                  :physics {:default "arcade"
-                            :arcade {:debug debug?
-                                     :gravity {:y gravity}}}
-                  :scale {:mode (-> phaser .-Scale .-ScaleModes .-FIT)
-                          :autoCenter (-> phaser .-Scale .-CENTER_BOTH),
-                          :width width,
-                          :height height},
-                  :scene (clj->js scenes)})]
+(defn gen-game [{:keys [debug? width height scenes boot gravity] :or {debug? false}}]
+  (let [sorted-scenes (sort-by #(if (= (-> %1 .-sys .-settings .-key) (name boot)) 0 1) scenes)
+        config (clj->js {:type (.-WEBGL phaser)
+                         :parent "root"
+                         :width (.-innerWidth js/window)
+                         :height (.-innerHeight js/window)
+                         :pixelArt false
+                         :physics {:default "arcade"
+                                   :arcade {:debug debug?
+                                            :gravity {:y gravity}}}
+                         :scale {:mode (-> phaser .-Scale .-ScaleModes .-FIT)
+                                 :autoCenter (-> phaser .-Scale .-CENTER_BOTH),
+                                 :width width,
+                                 :height height},
+                         :scene (clj->js sorted-scenes)})]
     (phaser/Game. config)))
 
 (defmethod ig/init-key :meowser/game [_ opts]
